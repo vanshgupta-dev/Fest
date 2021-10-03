f5064c58ae7664983c93e900bc2c3f9552219ea5
aravindh <aravindsaiylm@gmail.com>
2021-10-03 18:48:24 +0530

    added 3rd file

---
+++ gpay with if.py
@@ -0,0 +1,10 @@
+hrs = input("enter hours:")
+h = float(hrs)
+rate = input("enter the rate:")
+r = float(rate)
+if h <= 40 :
+    print(h*r)
+elif h > 40 :
+    print(40*r+(h-40)*1.5*r )   x = 'banana'
+y = max(x)
+print(y)
